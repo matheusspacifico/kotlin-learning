61c919ec1707bd52c824c864f32bc15f8a57e9a4
matheusspacifico <matheuspacifico44@gmail.com>
2025-03-12 14:40:21 -0300

    feat(first exercises): exercise 2

--- src/first/exercises/list1.kt
+++ src/first/exercises/list1.kt
@@ -1,11 +1,15 @@
 package first.exercises
 
 fun main() {
-    val inputs: List<Int> = listOf(100, 5, 20, -5)
-    for (n in inputs) {
+    val inputsEx1: List<Int> = listOf(100, 5, 20, -5)
+    for (n in inputsEx1) {
         println(exercicio1(n))
     }
     println("========================")
+    println(exercicio2(2, 3, 2))
+    println(exercicio2(2, 2, 2))
+    println(exercicio2(1, 2, 4))
+    println(exercicio2(-1, 2, 4))
 }
 
 fun exercicio1(qtdLivros: Int): String {
@@ -17,4 +21,18 @@ fun exercicio1(qtdLivros: Int): String {
     if (precoA == precoB) return "Indiferente"
     if (precoA > precoB) return "Critério A"
     return "Critério B"
+}
+
+fun exercicio2(ladoA: Int, ladoB: Int, ladoC: Int): String {
+    if (ladoA < 0 || ladoB < 0 || ladoC < 0) return "Erro"
+
+    if ((ladoA + ladoB) < ladoC) return "Não forma triângulo"
+    if ((ladoA + ladoC) < ladoB) return "Não forma triângulo"
+    if ((ladoB + ladoC) < ladoA) return "Não forma triângulo"
+
+    if (ladoA == ladoB && ladoB == ladoC) return "Equilátero"
+    if (ladoA == ladoB) return "Isósceles"
+    if (ladoA == ladoC) return "Isósceles"
+    if (ladoB == ladoC) return "Isósceles"
+    return "Escaleno"
 }
